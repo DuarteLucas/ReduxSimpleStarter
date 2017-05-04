bf5062f24ffcbe36c9abe0aaa847ff9f75714896
Duarte Lucas <duarteprista@gmail.com>
2017-05-04 14:46:54 +0100

    video_list_item.js and video_list basic components created

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,17 +5,17 @@ import React, { Component } from 'react';
 //   return <input />
 // };
 
-// - CLASS-based COMPONENT:
+// CLASS-based COMPONENT:
 // - added funcionality;
 // - must always definde a RENDER METHOD and return
 // some JSX otherwise you'll end up with an error
 class SearchBar extends Component {
   // this is how we initialize state in a class-based component
   // always called whenever we create an instance of SearchBar
-  constructor(props) {
+  Constructor(props) {
     // super calls the parent method
     super(props);
-    // - when creating a state we also create a object which
+    // When creating a state we also create a object which
     // passes properties we want to record
     // MANUALLY CHANGING STATE -- this.state = { object_properties}
     this.state = { term: '' };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 // - '.' before the path means 'in the current folder'
 // - don't need to include the file extension if it's
 // a '.js' file
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDOy40pU-caImdARxvBYsMmHuFmJ05xTok';
+
+
 
 // Create a new component. This component should produce
 // some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  Constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos }) -- when key and property are the same variable
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
+
 
 
 
